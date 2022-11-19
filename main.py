c2c3b01f62decbf8742e3e6382f01b4495179701
Veronika <vkotsubiak@gmail.com>
2022-11-19 16:52:14 +0200

    виводення

--- main.py
+++ main.py
@@ -7,3 +7,39 @@ cur = connection.cursor()
 #cur.execute('CREATE TABLE products (name TEXT, price TEXT, number TEXT)')
 #connection.commit()
 
+def pe():
+      global name
+      global name1
+      global price
+      global price1
+      global number
+      global r1
+      global r2
+      global r3
+      global row
+      global cur
+      if p =='3':
+            name = input('Enter name')
+            price = int(input('Enter price'))
+            number = int(input('Enter number'))
+            print('OK it is create')
+            cur.execute(f'INSERT INTO products (name, price, number) VALUES ("{name}" , "{price}" , "{number}")')
+            connection.commit()
+      if p =='2':
+            r1 = int(input('Enter row'))
+            cur.execute(f'DELETE FROM products WHERE rowid = {r1}')
+            connection.commit()
+      if p == '1':
+            print(res)
+      if p == '4':
+            name1 = input('Enter name')
+            r2 = int(input('Enter row'))
+            cur.execute(f'UPDATE products SET name = "{name1}" WHERE rowid = {r2}')
+            connection.commit()
+      if p == '5':
+            price1 = int(input('Enter price'))
+            r3 = int(input('Enter row'))
+            cur.execute(f'UPDATE products SET price = "{price1}" WHERE rowid = "{r3}"')
+            connection.commit()
+
+
